fd4a1252a0964a174d3051e31fdc809675277456
dmeneguin <meneguin.daniel@gmail.com>
2023-03-18 14:38:11 -0300

    modified port of downstream

--- shovel_create.py
+++ shovel_create.py
@@ -3,7 +3,7 @@ import requests
 RABBITMQ_USER='guest'
 RABBITMQ_PASS='guest'
 
-payload={"component":"shovel","vhost":"/","name":"asdd","value":{"src-uri":"amqp://","dest-uri":"amqp://guest:guest@rabbitmq_downstream","ack-mode":"on-confirm","src-protocol":"amqp091","dest-protocol":"amqp091","src-queue":"demo_queue","src-delete-after":"never","dest-queue":"demo_queue_2","dest-add-forward-headers":False}}
+payload={"component":"shovel","vhost":"/","name":"asdd","value":{"src-uri":"amqp://","dest-uri":"amqp://guest:guest@rabbitmq_downstream:5673","ack-mode":"on-confirm","src-protocol":"amqp091","dest-protocol":"amqp091","src-queue":"demo_queue","src-delete-after":"never","dest-queue":"demo_queue_2","dest-add-forward-headers":False}}
 
 client_auth = requests.auth.HTTPBasicAuth(RABBITMQ_USER,RABBITMQ_PASS)
 
